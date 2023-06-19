f1c18e058e11020629aeb86dfb4864b9cda922af
Hazal Onler <hazaltaneri@gmail.com>
2023-06-19 15:49:57 -0400

    Project name was changed according to boardname

--- src/components/Board/Header.js
+++ src/components/Board/Header.js
@@ -1,7 +1,12 @@
+import BoardClient from "../../store/BoardClient";
+
 const Header = () => {
+
+    const project = BoardClient.getBoardConfig();
+
     return (
         <div className="container-fluid bg-primary text-white p-3">
-            <h2 >Project Name</h2>
+            <h2 >{project.boardName}</h2>
         </div>
     );
 };
