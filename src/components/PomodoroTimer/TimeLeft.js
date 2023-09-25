23505fb8ad4ce176dba85b96702465380af93ad8
Hazal Onler <hazaltaneri@gmail.com>
2023-09-25 18:35:56 -0400

    Start button for pomodoro was added

--- src/components/PomodoroTimer/TimeLeft.js
+++ src/components/PomodoroTimer/TimeLeft.js
@@ -2,6 +2,7 @@ import React from "react";
 import moment from "moment";
 import momentDurationFormatSetup from "moment-duration-format";
 import { useState } from "react";
+import { useEffect } from "react";
 
 momentDurationFormatSetup(moment);
 
@@ -9,10 +10,34 @@ const TimeLeft = ({sessionLength}) => {
 
     const [timeLeft, setTimeLeft] = useState(sessionLength);
 
-    const formattedTimeLeft = moment.duration(timeLeft, 's').format('mm:ss');
+    // change timeLeft whenever sessionLength changes
+
+    useEffect (() => {
+        setTimeLeft(sessionLength);
+    }, [sessionLength])
+
+    const handleStartStopClick = () => {
+        // decrement timeleft by one every second (1000 ms)
+        // to do this we'll use setInterval
+
+        setInterval(() => {
+            setTimeLeft(prevTimeLeft => {
+                const newTimeLeft = prevTimeLeft - 1;
+                if (newTimeLeft >= 0) {
+                    return prevTimeLeft - 1 ;
+                } else {
+                    return prevTimeLeft;
+                }
+            });
+        }, 100)
+
+    };
+
+    const formattedTimeLeft = moment.duration(timeLeft, 's').format('mm:ss', { trim: false });
     return (
-        <div className="d-flex align-items-center pr-5">
+        <div className="d-flex flex-column align-items-center pr-5">
             {formattedTimeLeft}
+            <button onClick={handleStartStopClick}>Start</button>
         </div>
     );
 };
